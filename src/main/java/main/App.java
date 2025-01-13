5f17cad81fa3b69625eb95d82c263b69ea31857a
aaboeh <aapo.aarvala@gmail.com>
2025-01-13 23:20:37 +0200

    first commit

---
+++ src/main/java/main/App.java
@@ -0,0 +1,76 @@
+package main;
+
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Anna auton merkki: ");
+        String brand = sc.nextLine();
+        System.out.print("Anna auton malli: ");
+        String model = sc.nextLine();
+        
+        Car car = new Car(brand, model);
+
+        boolean exit = false;
+        while(!exit) {
+            System.out.println("1) Näytä auton tila");
+            System.out.println("2) Muokkaa auton merkkiä ja mallia");
+            System.out.println("3) Kiihdytä autoa");
+            System.out.println("4) Hidasta autoa");
+            System.out.println("0) Lopeta ohjelma");
+            System.out.println("Valitse toiminto: ");
+
+            if (sc.hasNext()) {
+                int i = 0;
+                String stringInput = sc.nextLine();
+                i = Integer.parseInt(stringInput);
+
+                switch(i) {
+                    case 1:
+                        car.carStatus();
+                        break;
+                    case 2:
+                        System.out.print("Anna uusi auton merkki: ");
+                        String newBrand = sc.nextLine();
+                        System.out.print("Anna uusi auton malli: ");
+                        String newModel = sc.nextLine();
+                        car.setBrand(newBrand);
+                        car.setModel(newModel);
+                        break;
+                    case 3:
+                        System.out.print("Kuinka monta km/h haluat kiihdyttää? ");
+                        String accString = sc.nextLine();
+                        int acceleration = Integer.parseInt(accString);
+                        if (acceleration > 0) {
+                            car.accelerate(acceleration);
+                        } else {
+                            System.out.println("Nopeuden täytyy olla positiivinen luku.");
+                        }
+                        break;
+                    case 4:
+                        System.out.print("Kuinka monta km/h haluat hidastaa? ");
+                        String decString = sc.nextLine();
+                        int deceleration = Integer.parseInt(decString);
+                        if (deceleration > 0) {
+                            car.decelerate(deceleration);
+                        } else {
+                            System.out.println("Nopeuden täytyy olla positiivinen luku.");
+                        }
+                        break;
+                    case 0:
+                        System.out.println("Kiitos ohjelman käytöstä.");
+                        exit = true;
+                        break;
+                    default:
+                        System.out.println("Syöte oli väärä. Yritä uudelleen.");
+                }
+            }
+        } 
+        sc.close();
+        
+    }
+}
---
+++ src/main/java/main/Car.java
@@ -0,0 +1,48 @@
+package main;
+
+public class Car {
+    private String brand;
+    private String model;
+    private int speed;
+
+    public Car(String brand, String model, int speed) {
+        this.brand = brand;
+        this.model = model;
+        this.speed = 0;
+    }
+
+    public void carStatus() {
+        System.out.println("Auto: " + brand + " " + model + ", Nopeus" + speed + " km/h");
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void accelerate(int acceleration) {
+        speed += acceleration;
+    }
+
+    public void decelerate(int deceleration) {
+        speed -= deceleration;
+        if (speed < 0) {
+            speed = 0;
+        }
+    }
+}
